47467da94b6ebb53742c94bc0d33887637765e53
Marin Visscher <m.r.visscher@cml.leidenuniv.nl>
2024-12-04 13:06:17 +0100

    Enable build to single file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
+import { viteSingleFile } from "vite-plugin-singlefile"
+
 // vite.config.js
 export default {
     "root": "src",
     "build": {
         "outDir": "../dist",
         "emptyOutDir": true,
-    }
+    },
+    "plugins": [viteSingleFile()],
   }
